0d5934463a00daac069cf98b4995de6e06126086
Fernandocalle9 <fernando.calle.q@Ucb.edu.bo>
2025-08-28 14:33:57 -0400

    agregue el ejercicio 2 y 3 de la practica

---
+++ PRACTICA_01/Ejercicio_01_02.cpp
@@ -0,0 +1,22 @@
+//Materia:Programacion I Paralelo 4
+//Autor:Fernando Antonio Calle Quispe 
+//Carnet:12573205 L.P.
+//Carrera del estudiante: Ingenieria Mecatronica
+// Fecha de creacion:28/08725
+//Numero de ejercicio :2
+#include <iostream>
+using namespace std;
+int main ()
+{
+int variable1=0, variable2=0;
+cout<<"valores iniciales: "<<endl;
+cout<<"variable="<<variable1<<endl;
+cout<<"variable2="<<variable2<<endl;
+cout <<"ingrese dos valores:";
+cin>> variable1>>variable2;
+
+cout<<"variable1 actual es="<<variable1<<endl;
+cout<<"variable2 actual es="<<variable2<<endl;
+
+return 0;
+}
---
+++ PRACTICA_01/Ejercicio_01_03.cpp
@@ -0,0 +1,19 @@
+//Materia:Programacion I Paralelo 4
+//Autor:Fernando Antonio Calle Quispe 
+//Carnet:12573205 L.P.
+//Carrera del estudiante: Ingenieria Mecatronica
+// Fecha de creacion:28/08725
+//Numero de ejercicio :3
+#include <iostream>
+using namespace std;
+int main ()
+{
+float base=0, altura=0;
+cout<< "ingrese la base:";
+cin>>base; 
+cout<< "ingrese la altura:";
+cin>>altura;
+float area = (base*altura)/2; 
+cout<<"el area de un trianguo es :"<<area;
+    return 0;
+}
