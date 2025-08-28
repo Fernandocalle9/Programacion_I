2a431149ebc49c882bbc07fb887804baff4602ec
Fernandocalle9 <fernando.calle.q@Ucb.edu.bo>
2025-08-28 14:25:08 -0400

    Adicione el ejercicio 1 de la practica

---
+++ PRACTICA_01/Ejercicio_01_01.cpp
@@ -0,0 +1,15 @@
+//Materia:Programacion I Paralelo 4
+//Autor:Fernando Antonio Calle Quispe 
+//Carnet:12573205 L.P.
+//Carrera del estudiante: Ingenieria Mecatronica
+// Fecha de creacion:28/08725
+//Numero de ejercicio :1
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout<<"Hola soy estudiante de programacion."<<endl;
+    cout<<"Mi nombre es Fernando Antonio Calle Quispe."<<endl;
+    cout<<"Mi carrera e Ingenieria Mecatronica."<<endl;
+     return 0;
+}
